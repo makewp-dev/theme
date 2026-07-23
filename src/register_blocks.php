be3c1495a0f4981afdb34d477bb2ed2155858c40
Taylor Gorman <taylor@thegorman.group>
2026-07-22 19:09:15 -0500

    rename autoload file

--- src/register_blocks.php
+++ src/register_blocks.php
@@ -21,9 +21,8 @@ function register_blocks()
     foreach( scandir( $dir_path ) as $filename )
     {
       $file_path = $dir_path . '/' . $filename;
-      if(
-        is_dir( $file_path )
-      ){
+      if( is_dir( $file_path ) )
+      {
         register_block_type( $file_path );
       }
     }
